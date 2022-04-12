eaaf67651439b2cf6e976793947099677a911f90
Thomas Boda <boda.thomas@orange.fr>
2022-04-12 16:49:39 +0200

    Create the BackButton component

---
+++ website/src/components/BackButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BackButton = ({onClick}) => {
+    return (
+        <button className="back-button" onClick={onClick}>Back</button>
+    );
+}
+
+export default BackButton;
